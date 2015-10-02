dc3d7eb9602d1e331be5b85ff18f8cd77d3203ff
baclap <clapperbrett@gmail.com>
2015-10-01 23:31:35 -0400

    Show post on home page

--- app/handlers/index.js
+++ app/handlers/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const Post = require('app/models/post');
+const r = require('thinky')().r;
+
 module.exports = {
   showIndex: function *(next) {
-    this.body = yield this.render('index');
+    const results = yield Post.run();
+    this.body = yield this.render('index',{
+      posts: results
+    });
   }
 };
